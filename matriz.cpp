48c1536579572c9bfcef24f34255df3dec518323
Dael Chávez Ferreyra <111296280+dwotV@users.noreply.github.com>
2024-08-23 09:13:22 -0600

    Update matriz.cpp

--- matriz.cpp
+++ matriz.cpp
@@ -3,27 +3,27 @@
 
 using namespace std;
 
-#define M 4
-#define N 4
+int M;
+int N;
 
 struct posicion {
   int x;
   int y;
 };
 
-void mostrarSolucion(int mat[M][N]) {
+void mostrarSolucion(int *mat) {
   for (int i = 0; i < M; i++) {
     for (int j = 0; j < N; j++) {
-      if (mat[i][j] == INT_MAX) {
+      if (mat[i*N+j] == INT_MAX) {
         cout << "0" << " ";
       } else {
-        if (mat[i][j] == 0) {
+        if (mat[i*N+j] == 0) {
           cout << "0" << " ";
         } else {
-          if (mat[i][j] != 0 && mat[i][j] != INT_MAX) {
+          if (mat[i*N+j] != 0 && mat[i*N+j] != INT_MAX) {
             cout << "1" << " ";
           } else {
-            cout << mat[i][j] << " ";
+            cout << mat[i*N+j] << " ";
           }
         }
       }
@@ -32,35 +32,35 @@ void mostrarSolucion(int mat[M][N]) {
   }
 }
 
-int esSolucion(struct posicion *solucion, int paso, int mat[M][N]) {
+int esSolucion(struct posicion *solucion, int paso, int *mat) {
   return (solucion[paso].x == M-1 && solucion[paso].y == N-1);
 }
 
-int generarCandidatos(struct posicion *solucion, struct posicion candidatos[], int mat[M][N], int paso) {
+int generarCandidatos(struct posicion *solucion, struct posicion candidatos[], int *mat, int paso) {
   int NC = 0;
   int X = solucion[paso - 1].x;
   int Y = solucion[paso - 1].y;
 
   //arriba
-  if(mat[X - 1][Y] == 0 && (X > 0)) {
+  if(mat[(X - 1)*N+Y] == 0 && (X > 0)) {
     candidatos[NC].x = X - 1;
     candidatos[NC].y = Y;
     NC++;
   }
   //abajo
-  if(mat[X + 1][Y] == 0 && (X < M - 1)) {
+  if(mat[(X + 1)*N+Y] == 0 && (X < M - 1)) {
     candidatos[NC].x = X + 1;
     candidatos[NC].y = Y;
     NC++;
   }
   //derecha
-  if(mat[X][Y + 1] == 0 && (Y < N - 1)) {
+  if(mat[X*N+(Y + 1)] == 0 && (Y < N - 1)) {
     candidatos[NC].x = X;
     candidatos[NC].y = Y + 1;
     NC++;
   }
   //izquierda
-  if(mat[X][Y - 1] == 0 && (Y > 0)) {
+  if(mat[X*N+(Y - 1)] == 0 && (Y > 0)) {
     candidatos[NC].x = X;
     candidatos[NC].y = Y - 1;
     NC++;
@@ -68,7 +68,7 @@ int generarCandidatos(struct posicion *solucion, struct posicion candidatos[], i
   return NC;
 }
 
-void backtracking(struct posicion *solucion, int paso, int mat[M][N]) {
+void backtracking(struct posicion *solucion, int paso, int *mat) {
   if (esSolucion(solucion, paso, mat)) {
     cout << "\nBacktracking:\n";
     mostrarSolucion(mat);
@@ -80,12 +80,12 @@ void backtracking(struct posicion *solucion, int paso, int mat[M][N]) {
     for (i = 0; i < NC; i++) {
       solucion[paso] = candidatos[i];
       //Bloquear
-      mat[solucion[paso].x][solucion[paso].y] = 1 + paso;
+      mat[solucion[paso].x * N + solucion[paso].y] = 1 + paso;
 
       backtracking(solucion, paso, mat);
       //Desbloquear
 
-      mat[solucion[paso].x][solucion[paso].y] = 0;
+      mat[solucion[paso].x * N + solucion[paso].y] = 0;
     }
   }
 }
@@ -94,27 +94,29 @@ int main (int argc, char *argv[]) {
 
   struct posicion solucion[M*N];
 
-  int mat[M][N];
+  cin >> M >> N;
+
+  int mat[M*N];
   for (int i = 0; i < M; i++) {
     for (int j = 0; j < N; j++) {
-      cin >> mat[i][j];
-      if (mat[i][j] == 0) {
-        mat[i][j] = INT_MAX;
+      cin >> mat[i*N+j];
+      if (mat[i*N+j] == 0) {
+        mat[i*N+j] = INT_MAX;
       }
     }
   }
 
   for (int i = 0; i < M; i++){
     for (int j = 0; j < N; j++) {
-      if (mat[i][j] == 1) {
-        mat[i][j] = 0;
+      if (mat[i*N+j] == 1) {
+        mat[i*N+j] = 0;
       }
     }
   }
 
   solucion[0].x = 0;
   solucion[0].y = 0;
-  mat[0][0] = 1;
+  mat[0] = 1;
 
   backtracking(solucion, 0, mat);
 
